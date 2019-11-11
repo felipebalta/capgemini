2aafa859dca484b66f16211619e4313f878cd858
felipebalta <33284445+felipebalta@users.noreply.github.com>
2019-11-11 09:50:43 -0300

    Add files via upload

---
+++ demoCapgemini/src/main/java/com/capgemini/demoCapgemini/DemoCapgeminiApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.demoCapgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCapgeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCapgeminiApplication.class, args);
+	}
+
+}
